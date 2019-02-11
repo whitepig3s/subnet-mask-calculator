4aac7d179b4c9e655028e4ca628eb2ff724cbe35
whitepig3s <whitepig3s@gmail.com>
2019-02-11 16:26:00 +0800

    Version 3.0

--- 1-cidr list.cpp
+++ 1-cidr list.cpp
@@ -6,6 +6,7 @@ void cidrlist(){
     char line[30];
     fstream fin;
     fin.open("CIDR.txt",ios::in);
+    cout<<"CIDR表示法 一般表示法 每個子網路HOST個數 子網路個數"<<endl;
     while(fin.getline(line,sizeof(line)))
         cout<<line<<endl;
     fin.close();
